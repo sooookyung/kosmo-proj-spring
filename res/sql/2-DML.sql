305327f974a53674f9a433e61132342b2a17543b
solban1 <solban1@daum.net>
2023-08-13 14:50:53 +0900

    Update DML.sql

--- res/sql/2-DML.sql
+++ res/sql/2-DML.sql
@@ -1,12 +1,12 @@
-insert into MEMBER (email, nickname, pwd, name, ph, age) values ('bhs123@bhs.com', 'admin', '1234', '°ü¸®ÀÚ', '01000000000', 30);
-insert into MEMBER (email, nickname, pwd, name, ph, age) values ('sol@bhs.com', 'sol', '1234', '¹è¼Ö¹İ', '01012341234', 28);
-insert into MEMBER (email, nickname, pwd, name, ph, age) values ('hsk@bhs.com', 'hsk', '1234', 'È«¼ö°æ', '01003568900', 20);
-insert into MEMBER (email, nickname, pwd, name, ph, age) values ('sang@bhs.com', 'sang', '1234', '¼­»ó·Ï', '01014702580', 25);
+insert into MEMBER (email, nickname, pwd, name, ph, age) values ('bhs123@bhs.com', 'admin', '1234', 'ê´€ë¦¬ì', '01000000000', 30);
+insert into MEMBER (email, nickname, pwd, name, ph, age) values ('sol@bhs.com', 'sol', '1234', 'ë°°ì†”ë°˜', '01012341234', 28);
+insert into MEMBER (email, nickname, pwd, name, ph, age) values ('hsk@bhs.com', 'hsk', '1234', 'í™ìˆ˜ê²½', '01003568900', 20);
+insert into MEMBER (email, nickname, pwd, name, ph, age) values ('sang@bhs.com', 'sang', '1234', 'ì„œìƒë¡', '01014702580', 25);
 
-insert into diner (name, location, category) values ('Ä«·¹', '¼ººÏ±¸', 'ÀÎµµ');
-insert into diner (name, location, category) values ('¾ß»óÇØ', '¿ë»ê±¸', 'Áß½Ä');
+insert into diner (name, location, category) values ('ì¹´ë ˆ', 'ì„±ë¶êµ¬', 'ì¸ë„');
+insert into diner (name, location, category) values ('ì•¼ìƒí•´', 'ìš©ì‚°êµ¬', 'ì¤‘ì‹');
 
-insert into board (title, content, writer, diner_seq, parent) values ('¿©±â ¸ÀÀÖÀ½', '¤¡¤¡', 'sol@bhs.com', 3, null);
-insert into board (title, content, writer, diner_seq, parent) values ('', '¤©¤·', 'sol@bhs.com', 3, 1);
+insert into board (title, content, writer, diner_seq, parent) values ('ì—¬ê¸° ë§›ìˆìŒ', 'ã„±ã„±', 'sol@bhs.com', 3, null);
+insert into board (title, content, writer, diner_seq, parent) values ('', 'ã„¹ã…‡', 'sol@bhs.com', 3, 1);
 
 alter table board drop constraint SYS_C009126;
