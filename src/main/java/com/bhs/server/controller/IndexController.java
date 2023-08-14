8180f239b31b27a00586c4a684d74c54ac47496b
solban1 <solban1@daum.net>
2023-08-14 11:35:32 +0900

    Add IndexController.java

--- src/main/java/com/bhs/server/controller/IndexController.java
+++ src/main/java/com/bhs/server/controller/IndexController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @GetMapping({"", "/"})
+    public String index() {
+        return "redirect:index.do";
+    }
 }
