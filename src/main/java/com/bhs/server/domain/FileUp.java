1bd692fcc64b72148163a829e62834a2793007fe
Evergreen3891 <songrok11@gmail.com>
2023-08-14 12:10:53 +0900

    update FileUp.java

---
+++ src/main/java/com/bhs/server/domain/FileUp.java
@@ -0,0 +1,36 @@
+package com.bhs.server.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Table(name = "file_up")
+@Entity
+public class FileUp {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private long id;
+    private String orgnm;
+    private String savednm;
+    private String savedpath;
+    private String contenttype;
+
+    @Builder
+    public FileUp(long id, String orgnm, String savednm, String savedpath, String contenttype) {
+        this.id = id;
+        this.orgnm = orgnm;
+        this.savednm = savednm;
+        this.savedpath = savedpath;
+        this.contenttype = contenttype;
+    }
+}
