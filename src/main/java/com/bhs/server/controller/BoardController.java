72be6e610f411237783235a27ec9d5f6b19bb8b3
solban1 <solban1@daum.net>
2023-08-16 10:45:44 +0900

    Add board/list

--- src/main/java/com/bhs/server/controller/BoardController.java
+++ src/main/java/com/bhs/server/controller/BoardController.java
@@ -2,15 +2,17 @@ package com.bhs.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
-    @GetMapping("board.do")
+    @GetMapping("/list.do")
     public String board() {
-        return "board";
+        return "list";
     }
 
-    @GetMapping("write.do")
+    @GetMapping("/write.do")
     public String write() {
         return "write";
     }
